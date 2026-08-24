b590dbc31b0e00b1a7140858ce36628ead9a873a
Arun Sanap <as3085034@gmail.com>
2026-08-24 19:50:29 +0530

    [121] [Time Beats: 99.96%] [Memory Beats: 15.58%] - LeetPush

---
+++ push code/121-Best-Time-to-Buy-and-Sell-Stock.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+
+            if (prices[i] < buyPrice) {
+                buyPrice = prices[i];
+            }
+
+            else {
+                int profit = prices[i] - buyPrice;
+
+                if (profit > maxProfit) {
+                    maxProfit = profit;
+                }
+            }
+        }
+
+        return maxProfit;
+    }
+        
+    }
+
+        
