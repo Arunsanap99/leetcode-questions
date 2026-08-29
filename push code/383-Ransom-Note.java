b4df2d95452020fbc4289016652d66a81b2fdafd
Arun Sanap <as3085034@gmail.com>
2026-08-29 22:07:40 +0530

    [383] [Time Beats: 99.96%] [Memory Beats: 67.91%] - LeetPush

---
+++ push code/383-Ransom-Note.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+        if (ransomNote.length() > magazine.length()) {
+            return false;
+        }
+
+        int[] freq = new int[26];
+
+        // Count characters available in magazine
+        for (int i = 0; i < magazine.length(); i++) {
+            freq[magazine.charAt(i) - 'a']++;
+        }
+
+        // Use characters for ransomNote
+        for (int i = 0; i < ransomNote.length(); i++) {
+            int index = ransomNote.charAt(i) - 'a';
+
+            freq[index]--;
+
+            if (freq[index] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
