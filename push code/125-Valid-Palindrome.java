86bd9fa0fba0c1e4b865dffb37c364888062280f
Arun Sanap <as3085034@gmail.com>
2026-08-31 09:46:18 +0530

    [125] [Time Beats: 44.65%] [Memory Beats: 73.75%] - LeetPush

--- push code/125-Valid-Palindrome.java
+++ push code/125-Valid-Palindrome.java
@@ -1,19 +1,17 @@
 class Solution {
     public boolean isPalindrome(String s) {
 
-       int left = 0;
+        int left = 0;
         int right = s.length() - 1;
 
         while (left < right) {
 
-            if(!Character.isLetterOrDigit(s.charAt(left))) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
                 left++;
-                continue;
             }
 
-            if( !Character.isLetterOrDigit(s.charAt(right))) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
                 right--;
-                continue;
             }
 
             if (Character.toLowerCase(s.charAt(left)) !=
