55c0e743c1ac33c378678d2e25260deda47cdc5e
Arun Sanap <as3085034@gmail.com>
2026-09-26 23:18:42 +0530

    [260] [Time Beats: 100.00%] [Memory Beats: 21.82%] - LeetPush

---
+++ push code/260-Single-Number-III.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+
+        int xor = 0;
+
+        // XOR all numbers
+        for (int num : nums) {
+            xor ^= num;
+        }
+
+        // Get the rightmost set bit
+        int diffBit = xor & -xor;
+
+        int a = 0;
+        int b = 0;
+
+        // Divide numbers into two groups
+        for (int num : nums) {
+
+            if ((num & diffBit) != 0) {
+                a ^= num;
+            } else {
+                b ^= num;
+            }
+        }
+
+        return new int[]{a, b};
+    }
+}
